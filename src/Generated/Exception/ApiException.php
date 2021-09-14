07a6f5f7839aa4b0c5491dcdeeb5c8d307db9cbb
sandra.kocic <sandra.kocic@datenkraft.com>
2021-09-14 08:17:52 +0200

    BB-1211 regenerate client

--- src/Generated/Exception/ApiException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Exception;
-
-interface ApiException extends \Throwable
-{
-}
--- src/Generated/Exception/ServerException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Exception;
-
-interface ServerException extends ApiException
-{
-}
