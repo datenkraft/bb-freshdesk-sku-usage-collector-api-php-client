ebb06a227d96dd2f3349e94f0ae4234cff3caea6
CircleCI <office@datenkraft.com>
2023-05-02 10:48:57 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/GetAuthRoleForbiddenException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Exception;
+
+class GetAuthRoleForbiddenException extends ForbiddenException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Forbidden');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
