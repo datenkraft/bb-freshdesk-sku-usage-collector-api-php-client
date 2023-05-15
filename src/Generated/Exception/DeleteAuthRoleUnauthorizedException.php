1ef0f3a933de8aed637a93c479e20ab0c4b3c5d4
CircleCI <office@datenkraft.com>
2023-05-15 16:07:19 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/DeleteAuthRoleUnauthorizedException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Exception;
+
+class DeleteAuthRoleUnauthorizedException extends UnauthorizedException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Unauthorized');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\FreshdeskSkuUsageCollectorApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
